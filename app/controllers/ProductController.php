ccd21a94a093a8f2aa9358bf93242eb41a4ba9a6
Francisco Mendoza <seecware@gmail.com>
2025-06-05 21:32:23 -0600

    Creating Controller based on BaseController, inheritance.
    Creating class, just to render static contect.

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -1,15 +1,12 @@
 <?php
 
-require_once __DIR__ . '/../models/product.php';
+require_once 'BaseController.php';
+require_once 'ViewRenderer.php';
 
-class ProductController {
-    public function index() {
-        echo "Productos";
-    }
-
-    public function searchProduct() {
-        $product = new Product;
-        echo $product -> getProductByBarCode("Product");
-        return;
+class ProductController extends BaseController
+{
+    public function __construct()
+    {
+        parent::__construct('products', __DIR__ . '/../views/products/', New ViewRenderer);
     }
 }
