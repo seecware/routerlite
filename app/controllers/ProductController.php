4239908cdad8553a06cec848ec2239374a402daf
Francisco Mendoza <seecware@gmail.com>
2025-06-06 10:27:23 -0600

    Converting require files to absolute paths.
    Also, changing the input parameter to match BaseController
    Class arguments.

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once 'BaseController.php';
-require_once 'ViewRenderer.php';
+require_once __DIR__ . '/BaseController.php';
+require_once __DIR__ . '/../core/ViewRenderer.php';
 
 class ProductController extends BaseController
 {
     public function __construct()
     {
-        parent::__construct('products', __DIR__ . '/../views/products/', New ViewRenderer);
+        parent::__construct('products', __DIR__ . '/../views/products/', New ViewRenderer, 'Products');
     }
 }
