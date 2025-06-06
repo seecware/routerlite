ff7cc7ec53abcc8f7a557f513831412757ab5223
Francisco Mendoza <seecware@gmail.com>
2025-06-06 10:25:57 -0600

    Adding new parameter for base claseController.
    This new parameter adds the title of a view to de constructor
    of abstract class BaseController.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -5,17 +5,19 @@ abstract class BaseController
     protected ViewRenderer $renderer;
     protected string $viewName;
     protected string $viewPath;
+    protected string $title;
 
-    public function __construct(string $viewName, string $viewPath, ViewRenderer $renderer)
+    public function __construct(string $viewName, string $viewPath, ViewRenderer $renderer, string $title)
     {
         $this->renderer = $renderer;
         $this->viewName = $viewName;
         $this->viewPath = $viewPath;
+        $this->title = $title;
     }
 
     public function index(): void
     {
-        $html = $this->renderer->render($this->viewName, $this->viewPath, []);
+        $html = $this->renderer->render($this->viewName, $this->viewPath, ['title' => $this->title]);
         $this->renderView($html);
     }
 
