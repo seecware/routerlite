406d32400458043a27b162a418b28c36d5cd01f2
Francisco Mendoza <seecware@gmail.com>
2025-06-06 10:31:17 -0600

    Adding new route match, and deleting unnused ones.

--- config/web.php
+++ config/web.php
@@ -1,6 +1,4 @@
 <?php
 
-$router->get('/', 'HomeController', 'index');
-$router->post('/login', 'AuthController', 'login');
 $router->get('/products', 'ProductController', 'index');
-$router-> get('/products/{bcode}', 'ProductController', 'searchProduct');
+$router->get('/error', 'RenderErrors', 'index');
