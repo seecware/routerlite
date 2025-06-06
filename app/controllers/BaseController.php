8d19e5f92f70fad1502bf2d6fc07f13bd4e80e08
Francisco Mendoza <seecware@gmail.com>
2025-06-05 21:33:37 -0600

    Creating base class for static content renderer.

---
+++ app/controllers/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+abstract class BaseController
+{
+    protected ViewRenderer $renderer;
+    protected string $viewName;
+    protected string $viewPath;
+
+    public function __construct(string $viewName, string $viewPath, ViewRenderer $renderer)
+    {
+        $this->renderer = $renderer;
+        $this->viewName = $viewName;
+        $this->viewPath = $viewPath;
+    }
+
+    public function index(): void
+    {
+        $html = $this->renderer->render($this->viewName, $this->viewPath, []);
+        $this->renderView($html);
+    }
+
+    protected function renderView(string $html): void
+    {
+        echo $html;
+    }
+}
